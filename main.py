4809291af9e5f6bd291705d20242672b0ea447f6
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2019-11-15 17:08:16 +0700

    2019-11-15

---
+++ main.py
@@ -0,0 +1,10 @@
+# just a test if the code can be written in MicroPython
+
+from machine import Pin
+from time import sleep
+
+led = Pin(2, Pin.OUT)
+
+while True:
+  led.value(not led.value())
+  sleep(0.5)
